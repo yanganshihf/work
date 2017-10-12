2badf96fc4a7d9bee518584c779a44e87f4b46ed
Administrator <Administrator@Jonathan>
2017-10-12 13:53:05 +0800

    ssm1012

--- src/main/java/com/how2java/util/Page.java
+++ src/main/java/com/how2java/util/Page.java
@@ -1,5 +1,12 @@
 package com.how2java.util;
- 
+
+/**
+ * 
+ * 分页类
+ *
+ * @author 杨岸石 17749262827
+ * @version 1.0, 2017年10月12日 下午1:37:49
+ */
 public class Page {
  
     int start=0;
@@ -24,6 +31,12 @@ public class Page {
         this.last = last;
     }
      
+    /**
+     * @Description: 计算末页
+     *
+     * @param total 设定文件
+     * void 返回类型
+     */
     public void caculateLast(int total) {
         // 假设总数是50，是能够被5整除的，那么最后一页的开始就是45
         if (0 == total % count)
@@ -32,5 +45,5 @@ public class Page {
         else
             last = total - total % count;       
     }
- 
+	
 }
